5e54bde859f4d6a3298cd29fde8ad2ce7bbb0e97
Максим Доценко <maxdocenko23@MacBook-Pro-Maksim.local>
2025-05-16 21:58:37 +0500

    commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable
 {
@@ -49,6 +50,7 @@ class User extends Authenticatable
     {
         return $this->getKey();
     }
+
     public function getJWTCustomClaims()
     {
         return [];
