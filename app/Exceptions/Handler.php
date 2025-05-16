7c3d3a314a0422a1b9798a4c6189b4d9291e842b
Максим Доценко <maxdocenko23@MacBook-Pro-Maksim.local>
2025-05-17 04:46:46 +0500

    commit

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Auth\Access\AuthorizationException;
+
 
 class Handler extends ExceptionHandler
 {
@@ -26,5 +28,22 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof AuthenticationException) {
+            return response()->json([
+                'error' => 'Unauthenticated.'
+            ], 401);
+        }
+
+        if ($exception instanceof AuthorizationException) {
+            return response()->json([
+                'error' => 'Forbidden. You do not have permission to access this resource.'
+            ], 403);
+        }
+
+        return parent::render($request, $exception);
     }
 }
